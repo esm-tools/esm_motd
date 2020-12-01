6a9cb48d34f0dfa3d106d677609d0d47479e4187
Dirk Barbi <dirk.barbi@awi.de>
2020-12-01 15:50:34 +0100

    Bump version: 1.0.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/esm-tools/esm_motd',
-    version='1.0.0',
+    version='2.0.0',
     zip_safe=False,
 )
